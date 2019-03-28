7fda95a973b5eb6a7996e120c4efc1901f5f6690
cachristopher <cory.christopher@octanner.com>
2019-03-28 12:13:42 -0600

    updated for new logout page

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const clientURI = process.env.CLIENT_URI || 'http://localhost:3000';
 const akkerisApi = process.env.AKKERIS_API;
 const authEndpoint = process.env.OAUTH_ENDPOINT;
 const https = require('https');
-const authPath = process.env.OAUTH_URI || '';
 
 const app = express();
 
@@ -89,20 +88,20 @@ app.get('/log-plex/:id', (req, res) => {
 });
 
 app.use((req, res, next) => {
-  if (req.session.token || req.path === '/oauth/callback') {
+  if (req.session.token || req.path === '/oauth/callback' || req.path === '/logout') {
     next();
   } else {
     req.session.redirect = req.originalUrl;
-    if(process.env.OAUTH_SCOPES) {
-      res.redirect(`${authEndpoint}${authPath}/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(`${clientURI}/oauth/callback`)}&scope=${encodeURIComponent(process.env.OAUTH_SCOPES)}`);
+    if (process.env.OAUTH_SCOPES) {
+      res.redirect(`${authEndpoint}/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(`${clientURI}/oauth/callback`)}&scope=${encodeURIComponent(process.env.OAUTH_SCOPES)}`);
     } else {
-      res.redirect(`${authEndpoint}${authPath}/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(`${clientURI}/oauth/callback`)}`);      
+      res.redirect(`${authEndpoint}/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(`${clientURI}/oauth/callback`)}`);   
     }
   }
 });
 
 app.get('/oauth/callback', (req, res) => {
-  const reqopts = { url: `${authEndpoint}${authPath}/access_token`, headers: { 'user-agent': 'akkerisui', accept: 'application/json' } };
+  const reqopts = { url: `${authEndpoint}/access_token`, headers: { 'user-agent': 'akkerisui', accept: 'application/json' } };
   reqopts.form = {
     client_id: clientID,
     client_secret: clientSecret,
@@ -133,12 +132,23 @@ app.use('/api', proxy(`${akkerisApi}`, {
 /* eslint-enable no-param-reassign */
 
 app.get('/logout', (req, res) => {
-  req.session.token = null;
-  res.redirect(`${authEndpoint}/logout`);
+  const reqopts = { url: `${authEndpoint}/authorizations/${req.session.token}`, headers: { 'user-agent': 'akkerisui', accept: 'application/json' } };
+  request.delete(reqopts, (error, response, body) => {
+    if (error) {
+      return console.error('delete failed:', error);
+    }
+    console.log('Logout successful!  Server responded with:', body);
+  });
+  req.session.destroy();
+  if (process.env.NODE_ENV === 'dev') {
+    res.sendFile(path.resolve('public', 'logout.html'));
+  } else {
+    res.sendFile(path.resolve('build', 'logout.html'));
+  }
 });
 
 app.get('/user', (req, res) => {
-  res.redirect(process.env.OAUTH_USER_VIEW || `${authEndpoint}${authPath}/user`);
+  res.redirect(process.env.OAUTH_USER_VIEW || `${authEndpoint}/user`);
 });
 
 if (process.env.NODE_ENV === 'dev') {
