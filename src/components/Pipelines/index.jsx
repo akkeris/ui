9578577e93d4db548ea93fb497fd08148888e9bf
Trevor Linton <trevor.linton@gmail.com>
2019-11-19 12:47:49 -0700

    COBRA-2944 - remove old new pipeline coupling

--- src/components/Pipelines/index.jsx
+++ src/components/Pipelines/index.jsx
@@ -1,3 +1,4 @@
 export { default as NewPipeline } from './NewPipeline';
+export { default as CreateOrUpdatePipelineCoupling } from './CreateOrUpdatePipelineCoupling';
+export { default as PipelinePromote } from './PipelinePromote';
 export { default as Stage } from './Stage';
-export { default as NewPipelineCoupling } from './NewPipelineCoupling';
